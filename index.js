5c7aff7601bead2b5ba76fd9b2f085d0bb427af0
Neil De Kock <100484177+nd0003@users.noreply.github.com>
2022-04-15 23:33:24 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function signIn() {
+  window.open(
+    "https://accounts.google.com/ServiceLogin?hl=en&passive=true&continue=https://www.google.com/&ec=GAZAmgQ"
+  );
+}
+
+function feelingLucky() {
+  window.open("https://www.google.com/doodles");
+}
+
+function sourceCode() {
+  window.open("https://github.com/nd0003/Google-Clone");
+}
